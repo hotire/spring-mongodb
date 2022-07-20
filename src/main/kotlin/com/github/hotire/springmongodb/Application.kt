88a729df4b4f09c369aa9220be8d4ae5a7a0f5e7
hotire <gngh0101@gmail.com>
2022-07-20 18:54:33 +0900

    Polish

--- src/main/kotlin/com/github/hotire/springmongodb/Application.kt
+++ src/main/kotlin/com/github/hotire/springmongodb/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringMongodbApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringMongodbApplication>(*args)
+    runApplication<Application>(*args)
 }
